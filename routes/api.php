de124e0ad2eff0cb6007b3cd9f0f9d0bde542089
WahyuAgg <wahyuagungbarokah@mail.ugm.ac.id>
2025-05-21 09:14:04 +0700

    add sanctum default auth

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,14 @@ use App\Http\Controllers\TopicController;
 use App\Http\Controllers\InvitationController;
 use App\Http\Controllers\UserProfileController;
 
+/*
+|--------------------------------------------------------------------------
+| Sanctum default Routes
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
 /*
